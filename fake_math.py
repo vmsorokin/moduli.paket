fcf7ba432a20fc365c2ee541b5985c351b3c0130
Vladimir <sorokin-v-m@yandex.ru>
2024-07-28 22:40:11 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if second == 0:
+        return ('Ошибка')
+    else: s = first/second
+    return s
---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as df
+from true_math import  divide as dt
+
+result1 = df(69, 3)
+result2 = df(3, 0)
+result3 = dt(49, 7)
+result4 = dt(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
